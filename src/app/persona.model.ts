d51349172e606239dbd7b0ba6ef12fea94c18f58
EdsonRego <edsonxrego@gmail.com>
2024-03-02 11:31:07 -0300

    Persona class created

---
+++ src/app/persona.model.ts
@@ -0,0 +1,17 @@
+export class Persona {
+    /*
+    nombre: String = '';
+    apellido: String = '';
+
+    constructor(nombre: String, apellido: String){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    */
+
+    //sintaxe simplificada
+    constructor(public nombre: String, public apellido: String) {
+
+    }
+
+}
