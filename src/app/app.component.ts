af4e303157d9168202d988ba1940c018d7e0b5b2
EdsonRego <edsonxrego@gmail.com>
2024-03-04 14:44:19 -0300

    Directiva @Output - Comunication from Father to Son Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,8 @@ export class AppComponent {
     new Persona('Laura', 'Juarez'),
     new Persona('Karla', 'Lara')
   ];
-  nombreInput: String = '';
-  apellidoInput: String = '';
 
-  agregarPersona() {
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personas.push(persona1);
+  personaAgregada(persona: Persona) {
+    this.personas.push(persona);
   }
 }
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Persona } from '../persona.model';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+
+  @Output() personaCreada = new EventEmitter<Persona>();
+
+
+  nombreInput: String = '';
+  apellidoInput: String = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personaCreada.emit(persona1);
+  }
 
 }
