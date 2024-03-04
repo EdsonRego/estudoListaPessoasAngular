65cde27355b76c3d8631a5fe73bc7991d57c28cd
EdsonRego <edsonxrego@gmail.com>
2024-03-04 17:37:06 -0300

    Services in Angular - setting Provider on component.ts

---
+++ src/app/LoggingService.service.ts
@@ -0,0 +1,7 @@
+export class LoggingService {
+
+    enviarMensagemAConsole(message: string) {
+        console.log(message);
+    }
+
+}
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,12 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model';
+import { LoggingService } from '../LoggingService.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css'
+  styleUrl: './formulario.component.css',
+  providers: [LoggingService]
 })
 export class FormularioComponent {
 
@@ -17,8 +19,12 @@ export class FormularioComponent {
   @ViewChild('nombreInput') nombreInput: ElementRef;
   @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
+  //Dependency Injection, via construtor
+  constructor(private loggingSevice: LoggingService) { }
+
   agregarPersona() {
     let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
+    this.loggingSevice.enviarMensagemAConsole("Enviamos persona con nombre: " + persona1.nombre + " apellido " + persona1.apellido);
     this.personaCreada.emit(persona1);
   }
 
