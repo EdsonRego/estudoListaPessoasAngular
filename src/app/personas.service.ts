68a1ecc30cf01d3f6b0186ee1e65faaf1da3a038
EdsonRego <edsonxrego@gmail.com>
2024-03-05 14:22:42 -0300

    Service Injection - @Injectable

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,13 +1,21 @@
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./LoggingService.service";
 import { Persona } from "./persona.model";
 
 
+@Injectable()
 export class PersonasService {
     personas: Persona[] = [
         new Persona('Juan', 'Perez'),
         new Persona('Laura', 'Juarez')
     ];
 
+    constructor(private loggingService: LoggingService) {
+
+    }
+
     agregarPersona(persona: Persona) {
+        this.loggingService.enviarMensagemAConsole("Agragamos persona: " + persona.nombre);
         this.personas.push(persona);
     }
 }
